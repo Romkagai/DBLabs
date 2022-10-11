cff185f03fb5a6c2ee765c9680267bd5ceb95a03
RomanPC <ludoviksoso@gmail.com>
2022-10-11 20:01:40 +0700

    test3

--- lab1/lab1.sql
+++ lab1/lab1.sql
@@ -1,23 +1,32 @@
 -----------
 /* 1 Список всех не пицц. Выборка должна содержать только наименование, цену и категорию товара. */
 
-SELECT product_name, category, price
-FROM pd_products
-WHERE category <> 'Пицца';
+SELECT
+    product_name, category, price
+FROM
+    pd_products
+WHERE
+    category NOT ILIKE 'Пицца';
 
 -----------
 /* 2 Список всех улиц и районов, в которые делались заказы. В списке не должно быть дублей.
    Выборка должна содержать только улицу и район. */
 
-SELECT DISTINCT street, area
-FROM pd_customers;
+SELECT DISTINCT
+    street, area
+FROM
+    pd_customers;
 -----------
 /* 3 Список всех продукты, в описании которых упоминается “Моцарелла”.
   Выборка должна содержать только наименование и описание.*/
 
-SELECT product_name, description
-FROM pd_products
-WHERE description ILIKE '%Моцарелла%';
+SELECT
+    product_name, description
+FROM
+    pd_products
+WHERE
+    description ILIKE '%Моцарелла%';
+
 -----------
 /*4 Список всех пицц, где для каждой подписана характеристика, является ли пицца острой или вегетарианской.
   Характеристика должны быть подписана по-русски, должен учитываться вариант выставления обоих отметок.
@@ -32,7 +41,7 @@ SELECT product_name, price,
 FROM
     pd_products
 WHERE
-    category = 'Пицца';
+    category ILIKE 'Пицца';
 
 -----------
 /* 5 Список всех сотрудников в формате: <Имя> должность “<название с маленькой буквы>”,
@@ -40,25 +49,31 @@ WHERE
 
 set lc_time = 'ru_RU';
 
-SELECT name || ', должность ' || lower(post) || ', работает с ' || to_char(start_date, 'TMMonth') ||
+SELECT
+    name || ', должность ' || lower(post) || ', работает с ' || to_char(start_date, 'TMMonth') ||
  ' ' || extract(year from start_date) || ' года' as employees
-from pd_employees;
+FROM
+    pd_employees;
 
 -----------
 /* 6 Список адресов покупателей, которые не указали район.
   Выборка должна представлять список адресов в формате <название улицы>, дом <номер дома> кв. <номер квартиры>. */
 
-SELECT street || ', дом ' || house_number || ', кв. ' || apartment as full_address
-FROM pd_customers WHERE area IS NULL;
+SELECT
+    street || ', дом ' || house_number || ', кв. ' || apartment as full_address
+FROM
+    pd_customers WHERE (area IS NULL or area = ' ');
 
 -----------
 /* 7 Выбрать все продукты без описания в категориях десерт и напитки.
   Выборка должна содержать только наименование, цену и категорию.*/
 
-SELECT product_name, price, category
-FROM pd_products
+SELECT
+    product_name, price, category
+FROM
+    pd_products
 WHERE
-    (description IS NULL OR description = '') and
+    (description IS NULL OR description = ' ') and
     (category ILIKE 'Десерты' OR
     category ILIKE 'Напитки');
 
@@ -66,21 +81,32 @@ WHERE
 /* 8 Список всех острых или вегетарианских пицц с базиликом ценой от 500.
   Выборка должна содержать только наименование, описание, категорию и цену.*/
 
-
-SELECT product_name, description, category, price
-FROM pd_products WHERE
-                     (is_vegan OR is_hot) AND
-                     price >= 500 AND
-                     description ILIKE '%Базилик%';
-
+SELECT
+    product_name, description, category, price
+FROM
+    pd_products
+WHERE
+    (is_vegan OR is_hot) AND
+    price >= 500 AND
+    description ILIKE '%Базилик%';
 
 -----------
-/* 9 Список всех острых пицц стоимостью от 460 до 510, если пицц при этом ещё и вегетарианская, то стоимость может доходить до 560.
+/* 9 Список всех острых пицц стоимостью от 460 до 510, если пицц при этом ещё и вегетарианская,
+   то стоимость может доходить до 560.
    Выборка должна содержать только наименование, цену и отметки об остроте и доступности для вегетарианцев.*/
 
-
-
-
+SELECT
+    product_name, price,
+    CASE WHEN is_hot AND is_vegan THEN 'Острая, Вегетарианская'
+            WHEN is_hot THEN 'Острая'
+            WHEN is_vegan THEN 'Вегетарианская'
+            ELSE 'Нет'
+        END AS characteristics
+FROM
+    pd_products
+WHERE
+    (category ILIKE 'Пицца' and is_hot and price between 460 and 510) or
+    (category ILIKE 'Пицца' and is_hot and is_vegan and price between 460 and 560);
 
 -----------
 /* 10 Для каждого продукта рассчитать, на сколько процентов можно поднять цену,
@@ -89,20 +115,23 @@ FROM pd_products WHERE
   процент повышения цены (округлить до 3-х знаков после запятой), размер возможного повышения до копеек.
  */
 
-SELECT product_name, price,
+SELECT
+    product_name, price,
+
+    --Берем первую цифру числа, увеличиваем на 1, умножаем её на количество разрядов этого числа и вычитаем единицу
+    --Затем используем эту же формулу для нахождения процента и разницы в цене
 
     cast((cast(substring(cast(price as text) from 1 for 1) as numeric) + 1) *
     power(10, length(cast (floor(price) as text)) - 1) - 1 as numeric) as new_price,
 
-    trunc(cast((cast(substring(cast(price as text) from 1 for 1) as numeric) + 1) *
-    power(10, length(cast (floor(price) as text)) - 1) - 1 as numeric) / price, 3) as "%",
+    (trunc((cast((cast(substring(cast(price as text) from 1 for 1) as numeric) + 1) *
+    power(10, length(cast (floor(price) as text)) - 1) - 1 as numeric) / price - 1) * 100 , 3))  as "%",
 
     (cast((cast(substring(cast(price as text) from 1 for 1) as numeric) + 1) *
-    power(10, length(cast (floor(price) as text)) - 1) - 1 as numeric) + 0.99) as prob_price
+    power(10, length(cast (floor(price) as text)) - 1) - 1 as numeric) - price) as diff_price
 
-FROM pd_products;
-
---ПЕРЕДЕЛАТЬ ПРОЦЕНТ
+FROM
+    pd_products;
 
 -----------
 /* 11 Список возможных цен на все продукты, если увеличить цену для острых продуктов на 1,5% , для вегетарианских на 1%, для острых и вегетарианских на 2%.
@@ -111,27 +140,36 @@ FROM pd_products;
   размер увеличения цены цену (до 2-х знаков после запятой) и отметки об остроте и доступности для вегетарианцев.
  */
 
-SELECT product_name, description, price,
-       trunc(price *
-             CASE
-                 WHEN is_hot AND is_vegan THEN 1.02
-                 WHEN is_hot THEN 1.015
-                 WHEN is_vegan THEN 1.01
-                 ELSE 1
-             END, 2) AS new_price,
+SELECT
+    product_name, description, price,
 
-       trunc(price *
-             CASE
-                 WHEN is_hot AND is_vegan THEN 0.02
-                 WHEN is_hot THEN 0.015
-                 WHEN is_vegan THEN 0.01
-                 ELSE 0
-             END, 2) AS add_price,
+    trunc(price *
+        CASE
+            WHEN is_hot AND is_vegan THEN 1.02
+            WHEN is_hot THEN 1.015
+            WHEN is_vegan THEN 1.01
+            ELSE 1
+        END, 2) AS new_price,
 
-        is_hot, is_vegan
+    trunc(price *
+        CASE
+            WHEN is_hot AND is_vegan THEN 0.02
+            WHEN is_hot THEN 0.015
+            WHEN is_vegan THEN 0.01
+            ELSE 0
+        END, 2) AS add_price,
 
-FROM pd_products
-    WHERE price *
+        CASE WHEN is_hot AND is_vegan THEN 'Острая, Вегетарианская'
+            WHEN is_hot THEN 'Острая'
+            WHEN is_vegan THEN 'Вегетарианская'
+            ELSE 'Нет'
+        END AS characteristics
+
+FROM
+    pd_products
+WHERE
+    price *
+
     CASE
         WHEN is_hot AND is_vegan THEN 1.02
         WHEN is_hot THEN 1.015
@@ -149,7 +187,8 @@ FROM pd_products
 /* 12 Список всех курьеров, которые выполняли заказы 1-го и 2-го января.
   Выборка должна содержать только Имя курьера и полный адрес заказа.*/
 
-SELECT pd_employees.name, street || ', дом ' || house_number || ', кв. ' || apartment as full_address
+SELECT
+    pd_employees.name, street || ', дом ' || house_number || ', кв. ' || apartment as full_address
 FROM pd_orders
     JOIN pd_employees ON emp_id = pd_employees.id
     JOIN pd_customers ON cust_id = pd_customers.id
@@ -162,7 +201,8 @@ WHERE
 /* 13 Список всех заказчиков, заказывавших пиццу в октябрьском районе в сентябре или октябре.
   Выборка должна содержать только имена покупателей без дублирования.*/
 
-SELECT DISTINCT pd_customers.name
+SELECT DISTINCT
+    pd_customers.name
 FROM pd_orders
     JOIN pd_customers ON pd_customers.id = pd_orders.cust_id
 WHERE
@@ -178,55 +218,52 @@ SELECT
     first.name || ', должность ' || lower(first.post) || ', работает с ' ||
     to_char(first.start_date, 'TMMonth') ||
     ' ' || extract(year from first.start_date) || ' года, непосредственный руководитель: ' ||
-    second.name
-
+    CASE
+        WHEN first.manager_id IS NULL THEN 'Отсутсвует'
+        ELSE second.name END AS employees
 FROM pd_employees first
-    JOIN pd_employees second ON first.manager_id = second.id;
+    LEFT JOIN pd_employees second ON first.manager_id = second.id;
 
 -----------
-/*Список всех адресов (без дублирования), которые были доставлены под руководством
+/* 15 Список всех адресов (без дублирования), которые были доставлены под руководством
   Барановой (или ей самой) зимой.
   В списке также должны отображаться: имя курьера, адрес, район (‘нет’ – если район не известен).
-  Выборка должна быть отсортирована по именам курьеров.*/
+  Выборка должна быть отсортирована по именам курьеров.
 
-SELECT DISTINCT pd_employees.name,
+upd. Ошибку понял, переделал выборку по "%Баранова%", а не по айдишникам
+
+*/
+
+SELECT DISTINCT firstTAB.name,
        street || ', дом ' || house_number || ', кв. ' || apartment as full_address,
-       CASE WHEN (area IS NULL ) THEN 'Нет' ELSE area END
+       CASE WHEN area IS NULL THEN 'Нет' ELSE area END
 FROM pd_orders
-    JOIN pd_employees ON pd_orders.emp_id = pd_employees.id
     JOIN pd_customers ON pd_orders.cust_id = pd_customers.id
+    JOIN pd_employees firstTAB ON pd_orders.emp_id = firstTAB.id
+    JOIN pd_employees secondTAB ON firstTAB.manager_id = secondTAB.id
+
 WHERE
-    (pd_employees.manager_id = 1 OR pd_employees.name ILIKE '%Баранова%') AND
+    (firstTAB.name ILIKE '%Баранова%' OR secondTAB.name ILIKE '%Баранова%') AND
     (extract(month from pd_orders.delivery_date) IN (1, 2, 12))
 ORDER BY
-    pd_employees.name ASC;
-
-ДОДЕЛАТЬ AREA ??
-
-    SELECT area from pd_customers
-where area = ' '
-
+    firstTAB.name ASC;
 
 -----------
-/*Список продуктов, которые заказывали вместе с острыми или вегетарианскими пиццами в этом месяце.*/
+/* 16 Список продуктов, которые заказывали вместе с острыми или вегетарианскими пиццами в этом месяце.*/
 
-SELECT *
-FROM pd_order_details
-    JOIN pd_orders ON pd_orders.id = pd_order_details.order_id
+SELECT DISTINCT
+    pd_products1.product_name
 
+FROM pd_orders
 
+    JOIN pd_order_details as pd_order_details1 ON pd_orders.id = pd_order_details1.order_id
+    JOIN pd_products as pd_products1 ON pd_order_details1.product_id = pd_products1.id
 
-
+    JOIN pd_order_details as pd_order_details2 ON pd_orders.id = pd_order_details2.order_id
+    JOIN pd_products as pd_products2 ON pd_order_details2.product_id = pd_products2.id
 
 WHERE
-    --(FIRST.product_id <> SECOND.product_id) and
-    extract(month from pd_orders.order_date) = extract(month from current_date);
-
-
-
-
-
-
-
-
-
+    pd_products1.category NOT ILIKE 'Пицца' and
+    (extract(month from pd_orders.order_date) = extract(month from current_date)) and
+    (pd_products2.is_hot or pd_products2.is_vegan) and
+    pd_products2.category ILIKE 'Пицца';
